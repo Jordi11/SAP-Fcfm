b24da89bfaead0d3875f638e24eb457ed6e263b5
JorDelgado <ne.ortega373@gmail.com>
2023-11-29 11:31:58 -0600

    Modificacion de ruta css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.scss";
+import "./scss/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="Nav-Header">
-          <a href="#Nosotros">
-            <img src="logo.png"></img>
-          </a>
-          <div className="section">
-            <a href="#Nosotros">Nosotros</a>
-            <a href="#Mision">Misión</a>
-            <a href="#Vision">Visión</a>
-            <a href="#Contacto">Contacto</a>
-          </div>
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="Nav-Header">
+        <a href="#Nosotros">
+          <img src="logo.png"></img>
+        </a>
+        <div className="section">
+          <a href="#Nosotros">Nosotros</a>
+          <a href="#Mision">Misión</a>
+          <a href="#Vision">Visión</a>
+          <a href="#Contacto">Contacto</a>
         </div>
-      </header>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
       <div className="">
         <h1>Sobre nosotros</h1>
         <section id="Nosotros">
@@ -56,6 +54,27 @@ function App() {
             info@grupoestudiantil.com
           </a>
         </section>
+        <section id="Proyectos">
+          <h2>Proyectos</h2>
+          <p>
+            Aquí puedes ver algunos de los proyectos en los que hemos estado
+            trabajando:
+          </p>
+          {/* Aquí puedes agregar imágenes o descripciones de tus proyectos */}
+        </section>
+        <section id="Colaboradores">
+          <h2>Colaboradores</h2>
+          <p>
+            Nuestro grupo estudiantil cuenta con la colaboración de varios
+            profesionales y expertos en el campo de la tecnología.
+          </p>
+          {/* Aquí puedes agregar imágenes o descripciones de tus colaboradores */}
+        </section>
+      </div>
+      <div className="Bottom-Bar">
+        <a href="#">Inicio</a>
+        <a href="#">Sobre nosotros</a>
+        <a href="#">Contacto</a>
       </div>
     </div>
   );
